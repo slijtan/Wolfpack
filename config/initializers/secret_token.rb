718cf834bca2ef7e550162dc4cd580f98b91794b
Lijen Tan <lijentan@gmail.com>
2011-02-20 11:24:38 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wolfpack::Application.config.secret_token = '1af153b34d9b18971f566656d8cf4c113aba5e4a14079846bf4d2938b094392609f1e35a084e5a8bd1cb7f3f08f8ce6b1b843e603ca45767781c8c396251c571'
