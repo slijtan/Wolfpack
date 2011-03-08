4599a7460e61410da046fff5173671a33ccaceb1
Lijen Tan <lijentan@gmail.com>
2011-03-07 22:42:32 -0800

    updating scripts format, run with rails runner ...

--- script/extract_email_html.rb
+++ script/extract_email_html.rb
@@ -1,22 +1,15 @@
-#!/usr/bin/env ruby
+require 'mail'
 
 # Arguments
 infile = ARGV[0]
-environment = ARGV[1] || 'development'
 
 if infile.nil?
   puts <<INFO
-Usage: extract_email_html.rb infile <environment>
+Usage: extract_email_html.rb infile
 Takes a raw email and prints out the html body decoded
 INFO
   exit
 end
-
-ENV['RAILS_ENV'] = environment
-SCRIPT_FOLDER = File.expand_path(File.join(File.dirname(__FILE__)))
-
-require File.join(SCRIPT_FOLDER, '../config/environment.rb')
-require 'mail'
 
 raw_email = File.new(infile, "r")
 mail = Mail.new(raw_email.read)
